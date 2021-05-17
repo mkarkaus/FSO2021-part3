ee70f2793e718901c2794c12411bceaa6261395b
mkarkaus <miikka.karkaus@gmail.com>
2021-05-17 14:17:23 +0300

    part3/a/ex02 done

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ app.get('/api/persons', (request, response) => {
 	response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+	const infoFor = '<p>Phonebook has info for ' + persons.length + ' people</p>'
+	const date = '<p>' + new Date + '</p>'
+	
+	response.send(infoFor + date)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
