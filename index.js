c44fe862853e122200139c41687c38a630834db2
mkarkaus <miikka.karkaus@gmail.com>
2021-05-25 13:50:08 +0300

    part3/c/ex13 and ex14 done

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 app.use(express.json())
-
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456"
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523"
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345"
-	},
-	{
-		id: 4,
-		name: "Mary Poppendick",
-		number: "39-23-6423122"
-	}
-]
 
 morgan.token('body', function(req, res) {
 	return JSON.stringify(req.body)
@@ -34,10 +13,10 @@ morgan.token('body', function(req, res) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
-
-const generateId = () => Math.floor(Math.random() * 100000) + 1
 
 app.post('/api/persons', (request, response) => {
 	const body = request.body
@@ -50,30 +29,22 @@ app.post('/api/persons', (request, response) => {
 		return response.status(400).json({
 			error: "Person's number is missing"
 		})
-	else if (persons.find(person => person.name === body.name))
-		return response.status(400).json({
-			error: "Person's name must be unique"
-		})
 	else
 	{
-		const newPerson = {
-			id: generateId(),
+		const person = new Person({
 			name: body.name,
 			number: body.number
-		}
-		persons = persons.concat(newPerson)
-		response.json(newPerson)
+		})
+		person.save().then(newPerson => {
+			response.json(newPerson)
+		})
 	}
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === Number(id))
-
-	if (person)
+	Person.findById(request.params.id).then(person => {
 		response.json(person)
-	else
-		response.status(404).end()
+	}).catch(err => response.status(404).end())
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -84,7 +55,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-	const infoFor = '<p>Phonebook has info for ' + persons.length + ' people</p>'
+	const infoFor = '<p>Phonebook has info for ' + Person.length + ' people</p>'
 	const date = '<p>' + new Date + '</p>'
 
 	response.send(infoFor + date)
@@ -97,7 +68,7 @@ const unknownEndpoint = (request, response) => {
 app.use(express.static('build'))
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
